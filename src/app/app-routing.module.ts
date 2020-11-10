61d81bbb49d85af6c340f3ca971e0cfd845ec211
Warren Tyler Beagle <tylerbeagle34@gmail.com>
2020-11-10 16:49:34 -0500

    Added home page, input content to home page, and updated colors

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 @NgModule({
