0dae66f79f8b540dc4d2033dd044a5f8283c1e33
Devon Harant <dharant@email.sc.edu>
2020-11-30 12:14:35 -0500

    added beginning camera functionality for #15

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
+  CameraPhoto, CameraSource } from '@capacitor/core';
+const { Camera, Filesystem, Storage } = Plugins;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri, 
+      source: CameraSource.Camera, 
+      quality: 100 
+    });
+  }
+}
