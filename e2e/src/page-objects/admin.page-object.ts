c981566e27abd898856508c4b4d4a925cfc7a699
Joshua Acree <45469478+jgacree@users.noreply.github.com>
2021-01-30 10:05:58 -0500

    Finalized behavioral testing for milestone

--- e2e/src/page-objects/admin.page-object.ts
+++ e2e/src/page-objects/admin.page-object.ts
@@ -1,10 +1,12 @@
 import { browser, element, by } from 'protractor';
 
+// Basic page object class, however, its not utilized currently
+// Will be utilized for future behavioral test
 export class AdminPageObject {
     navigateTo() {
         return browser.get('/admin-login');
     }
     getAdminPageTitle() {
-        return element(by.css('ion-card content')).getText();
+        return element(by.css('.ion-card content')).getText();
     }
 }
--- e2e/src/tab.e2e-spec.ts
+++ e2e/src/tab.e2e-spec.ts
@@ -1,32 +1,32 @@
 import { browser, element, by, ExpectedConditions } from 'protractor';
 
+//Behavioral Test that clicks through each tab and verifies that it is properly named
 describe('tabs', () => {
 
+  // Begins on home page
   beforeEach(() => {
-    //page = new AppPage();
     browser.get("/");
   });
 
   it('Tab 0 should be named Home', () => {
-    //page.navigateTo();
     expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Home"); 
   });
 
-  it('User can navigate to Tab 1 or the Find Seats Tab', async () => {
+  it('User can navigate to Tab 1 and it should be named Find Seats', async () => {
     await element(by.css("[tab=tab1]")).click();
 
     browser.driver.sleep(500);
     expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Find Seats");
   });
 
-  it('User can navigate to Tab 2 or the Maps Tab', async () => {
+  it('User can navigate to Tab 2 and it should be named Maps', async () => {
     await element(by.css("[tab=tab2]")).click();
 
     browser.driver.sleep(500);
     expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Maps");
   })
 
-  it('User can navigate to Tab 3 or the Info Tab', async () => {
+  it('User can navigate to Tab 3 and it should be name Info', async () => {
     await element(by.css("[tab=tab3]")).click();
 
     browser.driver.sleep(500);
