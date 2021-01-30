967cd5411d8477a576dc4bacc45390dd49f1dbed
ninjames01 <jamescimportant@yahoo.com>
2021-01-29 19:24:09 -0500

    working on parsing text from scan (#67) *not complete

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -9,11 +9,14 @@ import { createWorker } from 'tesseract.js';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  worker :Tesseract.Worker;
+  worker: Tesseract.Worker;
   workerReady = false;
   image = '';
   ocrResult = '';
   captureProgress = 0;
+  scan_section = '';
+  scan_row = '';
+  scan_seat = '';
 
   constructor(
     private router: Router,
@@ -25,7 +28,7 @@ export class Tab1Page {
   seat = {
     section: '',
     row: '',
-    seatNum: '' 
+    seatNum: ''
   }
 
   sections = [
@@ -34,10 +37,10 @@ export class Tab1Page {
 
   rows: string[];
 
-  ORCrows = [ 
+  ORCrows = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U'
   ]
-  GTRrows = [ 
+  GTRrows = [
     'AA', 'BB', 'CC', 'DD', 'EE', 'FF', 'GG', 'HH', 'II', 'JJ', 'KK', 'LL'
   ]
   BALrows = [
@@ -261,14 +264,14 @@ export class Tab1Page {
   }
 
   setRows(inputSection: string) {
-    if(this.seat.row!=null) {
+    if (this.seat.row != null) {
       this.removeRowSelection();
     }
-    if(inputSection=="RORC" || inputSection=="LORC") {
+    if (inputSection == "RORC" || inputSection == "LORC") {
       this.rows = this.ORCrows;
-    } else if(inputSection=="RGTR" || inputSection=="LGTR") {
+    } else if (inputSection == "RGTR" || inputSection == "LGTR") {
       this.rows = this.GTRrows;
-    } else if(inputSection=="RBAL" || inputSection=="LBAL") {
+    } else if (inputSection == "RBAL" || inputSection == "LBAL") {
       this.rows = this.BALrows;
     } else {
       this.rows = ['HC'];
@@ -276,82 +279,82 @@ export class Tab1Page {
   }
 
   setSeats(inputRow: string) {
-    if(this.seat.seatNum!=null) {
+    if (this.seat.seatNum != null) {
       this.removeSeatSelection();
     }
-    if (inputRow=="A") {
+    if (inputRow == "A") {
       this.seats = this.Aseats;
-    } else if(inputRow=="B") {
+    } else if (inputRow == "B") {
       this.seats = this.Bseats;
-    } else if(inputRow=="C") {
+    } else if (inputRow == "C") {
       this.seats = this.Cseats;
-    } else if(inputRow=="D") {
+    } else if (inputRow == "D") {
       this.seats = this.Dseats;
-    } else if(inputRow=="E") {
+    } else if (inputRow == "E") {
       this.seats = this.Eseats;
-    } else if(inputRow=="F") {
+    } else if (inputRow == "F") {
       this.seats = this.Fseats;
-    } else if(inputRow=="G") {
+    } else if (inputRow == "G") {
       this.seats = this.Gseats;
-    } else if(inputRow=="H") {
+    } else if (inputRow == "H") {
       this.seats = this.Hseats;
-    } else if(inputRow=="I") {
+    } else if (inputRow == "I") {
       this.seats = this.Iseats;
-    } else if(inputRow=="J") {
+    } else if (inputRow == "J") {
       this.seats = this.Jseats;
-    } else if(inputRow=="K") {
+    } else if (inputRow == "K") {
       this.seats = this.Kseats;
-    } else if(inputRow=="L") {
+    } else if (inputRow == "L") {
       this.seats = this.Lseats;
-    } else if(inputRow=="M") {
+    } else if (inputRow == "M") {
       this.seats = this.Mseats;
-    } else if(inputRow=="N") {
+    } else if (inputRow == "N") {
       this.seats = this.Nseats;
-    } else if(inputRow=="O") {
+    } else if (inputRow == "O") {
       this.seats = this.Oseats;
-    } else if(inputRow=="P") {
+    } else if (inputRow == "P") {
       this.seats = this.Pseats;
-    } else if(inputRow=="Q") {
+    } else if (inputRow == "Q") {
       this.seats = this.Qseats;
-    } else if(inputRow=="R") {
+    } else if (inputRow == "R") {
       this.seats = this.Rseats;
-    } else if(inputRow=="S") {
+    } else if (inputRow == "S") {
       this.seats = this.Sseats;
-    } else if(inputRow=="T") {
+    } else if (inputRow == "T") {
       this.seats = this.Tseats;
-    } else if(inputRow=="U") {
+    } else if (inputRow == "U") {
       this.seats = this.Useats;
-    } else if(inputRow=="AA") {
+    } else if (inputRow == "AA") {
       this.seats = this.AAseats;
-    } else if(inputRow=="BB") {
+    } else if (inputRow == "BB") {
       this.seats = this.BBseats;
-    } else if(inputRow=="CC") {
+    } else if (inputRow == "CC") {
       this.seats = this.CCseats;
-    } else if(inputRow=="DD") {
+    } else if (inputRow == "DD") {
       this.seats = this.DDseats;
-    } else if(inputRow=="EE") {
+    } else if (inputRow == "EE") {
       this.seats = this.EEseats;
-    } else if(inputRow=="FF") {
+    } else if (inputRow == "FF") {
       this.seats = this.FFseats;
-    } else if(inputRow=="GG") {
+    } else if (inputRow == "GG") {
       this.seats = this.GGseats;
-    } else if(inputRow=="HH") {
+    } else if (inputRow == "HH") {
       this.seats = this.HHseats;
-    } else if(inputRow=="II") {
+    } else if (inputRow == "II") {
       this.seats = this.IIseats;
-    } else if(inputRow=="JJ") {
+    } else if (inputRow == "JJ") {
       this.seats = this.JJseats;
-    } else if(inputRow=="KK") {
+    } else if (inputRow == "KK") {
       this.seats = this.KKseats;
-    } else if(inputRow=="LL") {
+    } else if (inputRow == "LL") {
       this.seats = this.LLseats;
-    } else if(inputRow=="AAA") {
+    } else if (inputRow == "AAA") {
       this.seats = this.AAAseats;
-    } else if(inputRow=="BBB") {
+    } else if (inputRow == "BBB") {
       this.seats = this.BBBseats;
-    } else if(inputRow=="CCC") {
+    } else if (inputRow == "CCC") {
       this.seats = this.CCCseats;
-    } else if(inputRow=="DDD") {
+    } else if (inputRow == "DDD") {
       this.seats = this.DDDseats;
     } else {
       this.seats = ['Invalid Section/Row']
@@ -359,17 +362,18 @@ export class Tab1Page {
   }
 
   removeRowSelection() {
-    this.seat.row=null;
-    this.seat.seatNum=null;
+    this.seat.row = null;
+    this.seat.seatNum = null;
   }
 
   removeSeatSelection() {
-    this.seat.seatNum=null;
+    this.seat.seatNum = null;
   }
 
-  async newCapture(){
+  async newCapture() {
     let capturedImage = await this.photoService.addNewToGallery();
     this.image = capturedImage;
+    // this.image = '../assets/img/ticket.jpg';
     this.recognizeImage();
   }
 
@@ -392,7 +396,30 @@ export class Tab1Page {
     const result = await this.worker.recognize(this.image);
     console.log(result);
     this.ocrResult = result.data.text;
+    console.log(result.data);
     console.log(this.ocrResult);
-  }
 
+    for (var i = 0; i < result.data.words.length; i++) {
+      if (result.data.words[i].text == 'Arts') {
+        console.log('Section ' + result.data.words[i + 1].text);
+        console.log('Row ' + result.data.words[i + 2].text);
+        console.log('Seat ' + result.data.words[i + 3].text);
+        this.scan_section = result.data.words[i + 1].text;
+        this.scan_row = result.data.words[i + 2].text;
+        this.scan_seat = result.data.words[i + 3].text;
+        break;
+      }
+    }
+
+    if (this.sections.includes(this.scan_section)) {
+      console.log("section test passed!");
+      if (this.scan_section == "RORC" || this.scan_section == "LORC") {
+      } else if (this.scan_section == "RGTR" || this.scan_section == "LGTR") {
+      } else if (this.scan_section == "RBAL" || this.scan_section == "LBAL") {
+        if (this.BALrows.includes(this.scan_row)) {
+          console.log("row test passed!");
+        }
+      }
+    }
+  }
 }
