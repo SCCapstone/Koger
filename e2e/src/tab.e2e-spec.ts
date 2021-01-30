6c80c0743eb645ad3023463120990152ee6969ba
Joshua Acree <45469478+jgacree@users.noreply.github.com>
2021-01-30 11:01:13 -0500

    Added comments and first part of #69

--- e2e/src/tab.e2e-spec.ts
+++ e2e/src/tab.e2e-spec.ts
@@ -8,10 +8,12 @@ describe('tabs', () => {
     browser.get("/");
   });
 
+  //Begins on Tab 0 and checks title
   it('Tab 0 should be named Home', () => {
     expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Home"); 
   });
 
+  //Begins from Tab 0 clicks tab 1 and checks title
   it('User can navigate to Tab 1 and it should be named Find Seats', async () => {
     await element(by.css("[tab=tab1]")).click();
 
@@ -19,6 +21,7 @@ describe('tabs', () => {
     expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Find Seats");
   });
 
+  //Begins from Tab 0 clicks tab 2 and checks title
   it('User can navigate to Tab 2 and it should be named Maps', async () => {
     await element(by.css("[tab=tab2]")).click();
 
@@ -26,6 +29,7 @@ describe('tabs', () => {
     expect(element(by.css(".tab-selected ion-label")).getText()).toContain("Maps");
   })
 
+  //Begins from Tab 0 clicks tab 3 and checks title
   it('User can navigate to Tab 3 and it should be name Info', async () => {
     await element(by.css("[tab=tab3]")).click();
 
