423ea0ac4ebfde0803c35adcf2b62f0c2c040562
Joshua Acree <45469478+jgacree@users.noreply.github.com>
2021-04-12 15:29:53 -0500

    Added some stuff for form validation for creating events #156 #155 also fixed bug for #165 where title can't be updated at all, due to the functionality of the entire app

--- src/app/create-event/create-event.page.ts
+++ src/app/create-event/create-event.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder,  Validators} from '@angular/forms';
+import { FormGroup, FormBuilder,  Validators, FormControl} from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoadingController, AlertController } from '@ionic/angular';
 import { FirestoreService } from '../services/data/firestore.service';
@@ -10,11 +10,12 @@ import { FirestoreService } from '../services/data/firestore.service';
 })
 export class CreateEventPage implements OnInit {
   public createEventForm: FormGroup;
+  
   constructor(
     public loadingCtrl: LoadingController,
     public alertCtrol:AlertController, 
     private firestoreService: FirestoreService,
-    formBuilder: FormBuilder, 
+    private formBuilder: FormBuilder,
     private router: Router
     ) { 
       this.createEventForm = formBuilder.group({
@@ -26,6 +27,19 @@ export class CreateEventPage implements OnInit {
     });
   }
   ngOnInit() {
+    this.createEventForm = this.formBuilder.group({
+      eventName: new FormControl('', Validators.compose([
+        Validators.required,
+        // Pattern must match ___@___.___
+        // Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      eventDescription: new FormControl('', Validators.compose
+      ([
+        Validators.minLength(1),
+        Validators.required
+      ])),
+      
+    });
   }
   async createEvent() {  
     const loading = await this.loadingCtrl.create();
@@ -41,7 +55,7 @@ export class CreateEventPage implements OnInit {
     then(
       () => {
         loading.dismiss().then(()=> {
-          this.router.navigateByUrl('');
+          this.router.navigateByUrl('edit-event');
         });
       },
       error => {
